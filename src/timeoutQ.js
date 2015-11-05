1d1c7e801471285059efee09deb6c5937246dfbe
Alexander Koshelev <akoshelev@users.noreply.github.com>
2015-11-04 23:38:35 -0800

    Initial code for defer & spec

--- src/timeoutQ.js
+++ src/timeoutQ.js
@@ -4,51 +4,67 @@
 angular
   .module('myApp', [])
   .config(function($provide) {
-    console.log('module run');
-    $provide.decorator('$q', function($delegate) {
+    var TIMEOUT_INTERVAL = 5 * 1000;
+    $provide.decorator('$q', function($delegate, $rootScope) {
       // Extend promises with non-returning handlers
-      function decoratePromise(promise) {
-        promise._then = promise.then;
-        promise.then = function(thenFn, errFn, notifyFn) {
-          var p = promise._then(thenFn, errFn, notifyFn);
-          return decoratePromise(p);
-        };
+      //function decoratePromise(promise) {
+      //  promise._then = promise.then;
+      //  promise.then = function(thenFn, errFn, notifyFn) {
+      //    var p = promise._then(thenFn, errFn, notifyFn);
+      //    return decoratePromise(p);
+      //  };
+      //
+      //  promise.success = function(fn) {
+      //    promise.then(function(value) {
+      //      fn(value);
+      //    });
+      //    return promise;
+      //  };
+      //  promise.error = function(fn) {
+      //    promise.then(null, function(value) {
+      //      fn(value);
+      //    });
+      //    return promise;
+      //  };
+      //  return promise;
+      //}
+      //
+      //var defer = $delegate.defer,
+      //  when = $delegate.when,
+      //  reject = $delegate.reject,
+      //  all = $delegate.all;
+      //$delegate.defer = function() {
+      //  var deferred = defer();
+      //  decoratePromise(deferred.promise);
+      //  return deferred;
+      //};
+      //$delegate.when = function() {
+      //  var p = when.apply(this, arguments);
+      //  return decoratePromise(p);
+      //};
+      //$delegate.reject = function() {
+      //  var p = reject.apply(this, arguments);
+      //  return decoratePromise(p);
+      //};
+      //$delegate.all = function() {
+      //  var p = all.apply(this, arguments);
+      //  return decoratePromise(p);
+      //};
 
-        promise.success = function(fn) {
-          promise.then(function(value) {
-            fn(value);
-          });
-          return promise;
-        };
-        promise.error = function(fn) {
-          promise.then(null, function(value) {
-            fn(value);
-          });
-          return promise;
-        };
-        return promise;
-      }
+      var defer = $delegate.defer;
 
-      var defer = $delegate.defer,
-        when = $delegate.when,
-        reject = $delegate.reject,
-        all = $delegate.all;
       $delegate.defer = function() {
-        var deferred = defer();
-        decoratePromise(deferred.promise);
-        return deferred;
-      };
-      $delegate.when = function() {
-        var p = when.apply(this, arguments);
-        return decoratePromise(p);
-      };
-      $delegate.reject = function() {
-        var p = reject.apply(this, arguments);
-        return decoratePromise(p);
-      };
-      $delegate.all = function() {
-        var p = all.apply(this, arguments);
-        return decoratePromise(p);
+
+        var dfd = defer();
+        setTimeout(function() {
+          var error = new Error('Promise rejected due to a timeout.');
+          console.error(error);
+          $rootScope.$apply(function() {
+            dfd.reject(error);
+          });
+        }, TIMEOUT_INTERVAL);
+
+        return dfd;
       };
 
       return $delegate;
--- test/timeoutQSpec.js
+++ test/timeoutQSpec.js
@@ -6,13 +6,34 @@ describe('timeoutQ', function() {
 
   'use strict';
 
-  var $timeout, $q;
+  var $interval,
+    $q,
+    clock;
 
   beforeEach(module('myApp'));
 
-  beforeEach(inject(function(_$timeout_, _$q_) {
-    $timeout = _$timeout_;
+  beforeEach(inject(function(_$interval_, _$q_) {
+    $interval = _$interval_;
     $q = _$q_;
   }));
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+  });
+
+  afterEach(function() {
+    clock && clock.restore();
+  });
+
+  it('test', function() {
+    // Arrange
+    var promise = $q.defer().promise;
+
+    // Act
+    clock.tick(5000);
+
+    // Assert
+    return promise.should.be.rejected && $interval.flush();
+  });
 
 });
